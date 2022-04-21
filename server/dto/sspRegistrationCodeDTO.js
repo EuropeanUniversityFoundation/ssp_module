1de1e7fc8b4ff4208d2aadb353094d97efb3d4da
Bruno Pereira <bdpereira@uporto.pt>
2022-04-21 17:52:30 +0100

    commit more functionalities for ssp provider registration

--- server/dto/sspRegistrationCodeDTO.js
+++ server/dto/sspRegistrationCodeDTO.js
@@ -4,8 +4,12 @@ module.exports = class SSPRegistrationCodeDTO {
     constructor(code, email) {
         this._code = code
         this._email = email
+
+        var date = new Date();
+        date.setDate(date.getDate() + 7);
+        this._expirationDate = date.toISOString()
     }
-   
+
     get code() {
         return this._code
     }
@@ -14,11 +18,16 @@ module.exports = class SSPRegistrationCodeDTO {
         return this._email
     }
 
+    get expDate(){
+        return this._expirationDate
+    }
+
 
     toJSON() {
         return {
             _id: this._code,
             email: this._email,
+            expirationDate: this._expirationDate    
         }
     }
 }
