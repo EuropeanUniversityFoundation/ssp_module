53974b1da874232157ee2333fa53a59020674031
Bruno Pereira <bdpereira@uporto.pt>
2022-04-22 11:37:38 +0100

    update connection string

--- server/model/constants/database.js
+++ server/model/constants/database.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DBURL: "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb",
+    DBURL: "mongodb://127.0.0.1:27017/",
     DBNAME: "ssp",
 
     TABLE_SSP_CODES: "ssp_codes",
