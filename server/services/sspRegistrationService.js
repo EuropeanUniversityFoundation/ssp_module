8d50ce081d564a7c435e810cb79725b6691aae3b
Bruno Pereira <bdpereira@uporto.pt>
2022-04-28 15:49:02 +0100

    log

--- server/services/sspRegistrationService.js
+++ server/services/sspRegistrationService.js
@@ -180,6 +180,7 @@ module.exports = {
 
     async calculateCertHash(name, newCode, callback) {
 
+        console.log(__dirname + '/certificates/' + name.replace(" ", "-") + '_' + newCode + '.crt');
         Utils.readFile(__dirname + '/certificates/' + name.replace(" ", "-") + '_' + newCode + '.crt', function (err, b64string) {
             var remadePublicKey = b64string.substring(b64string.indexOf("\n") + 1)
             var remadePublicKey2 = remadePublicKey.replace("-----END CERTIFICATE-----", "")
