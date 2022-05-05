01a8b505f3934adb84a5b8557c7d9584bf288f0a
Bruno Pereira <bdpereira@uporto.pt>
2022-05-05 18:37:18 +0100

    fix

--- server/services/certificates/generate_certificate.sh
+++ server/services/certificates/generate_certificate.sh
@@ -4,4 +4,4 @@ openssl genrsa -out ""$1"_"$6"".key --passout pass:"$2" 4096
 echo openssl req -new -key ""$1"_"$6"".key --passin pass:"$2" -out ""$1"_"$6"".csr -subj "/C="$3"/ST="$4"/L="$4"/O=EDSSI/OU=EDSSI/CN=edssiregistrationportal.it.auth.gr" -addext "subjectAltName = DNS:www.edssiregistrationportal.it.auth.gr" 
 openssl req -new -key ""$1"_"$6"".key --passin pass:"$2" -out ""$1"_"$6"".csr -subj "/C="$3"/ST="$4"/L="$4"/O=EDSSI/OU=EDSSI/CN=edssiregistrationportal.it.auth.gr" -addext "subjectAltName = DNS:www.edssiregistrationportal.it.auth.gr" 
 echo openssl x509 -req -days 365 -in ""$1"_"$6"".csr -CA providerca.crt --passin pass:"$2" -CAkey  providerca.key -set_serial "$5" -out ""$1"_"$6"".crt
-openssl x509 -req -days 365 -in ""$1"_"$6"".csr -CA providerca.crt --passin pass:"$2" -CAkey  providerca.key -set_serial "$5" -out ""$1"_"$6"".crt
+openssl x509 -req -days 365 -in ""$1"_"$6"".csr -CA providerca.crt --passin pass:"$7" -CAkey  providerca.key -set_serial "$5" -out ""$1"_"$6"".crt
