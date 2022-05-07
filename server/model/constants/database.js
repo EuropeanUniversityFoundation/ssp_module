b1713556ea4222963da016fa6e960d14600a5478
Bruno Pereira <bdpereira@uporto.pt>
2022-05-07 14:59:31 +0100

    database variables

--- server/model/constants/database.js
+++ server/model/constants/database.js
@@ -1,8 +1,11 @@
 module.exports = Object.freeze({
-    // DBURL: "mongodb+srv://ssp:Sezcs580jhmfSg1z@cluster0.kinfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    //DBURL: "mongodb+srv://ssp:Sezcs580jhmfSg1z@cluster0.kinfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     DBURL: "mongodb://mongo-db:27017/",
     DBNAME: "test",
 
     TABLE_SSP_CODES: "ssp_codes",
-    TABLE_SSP_PROVIDERS: "ssp_providers"
+    TABLE_SSP_PROVIDERS: "ssp_providers",
+    TABLE_INST_AND_PROVIDERS: "institutions_and_providers",
+    TABLE_SERVICE_TYPE: "service_type",
+    TABLE_INSTITUTION_OWN_INFORMATION: "inst_own_information",
 });
