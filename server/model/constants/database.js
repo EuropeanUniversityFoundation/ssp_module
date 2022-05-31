1b8fac73d68ef73219b32a4eb4c4fabb08451790
Bruno Pereira <bdpereira@uporto.pt>
2022-05-31 09:37:31 +0100

    revert

--- server/model/constants/database.js
+++ server/model/constants/database.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DBURL: "mongodb+srv://ssp:Sezcs580jhmfSg1z@cluster0.kinfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    //DBURL: "mongodb://mongo-db:27017/",
+    // DBURL: "mongodb+srv://ssp:Sezcs580jhmfSg1z@cluster0.kinfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    DBURL: "mongodb://mongo-db:27017/",
     DBNAME: "test",
 
     TABLE_SSP_CODES: "ssp_codes",
