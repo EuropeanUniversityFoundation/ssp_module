21957eb795027be1adb20a9b42474e406b8cd187
Bruno Pereira <bdpereira@uporto.pt>
2022-07-20 17:08:43 +0100

    commit server alterations

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -9,32 +10,45 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+const sslvalidation = express();
 
 const port = process.env.PORT || 8553;
+const sslPort = 8554;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use((bodyParser.urlencoded({ extended: true })));
+sslvalidation.use(bodyParser.json());
+sslvalidation.use((bodyParser.urlencoded({ extended: true })));
 
-// https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, './certs/privkey.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, './certs/fullchain.pem')),
-// }, app
-// ).listen(port);
+sslvalidation.use(cors());
+sslvalidation.use(bodyParser.json());
+sslvalidation.use((bodyParser.urlencoded({ extended: true })));
+
+https.createServer({
+    cert: fs.readFileSync(path.join(__dirname, './services/certificates/sspfullchain.pem')),
+    key: fs.readFileSync(path.join(__dirname, './services/certificates/sspprivkey.pem')),
+    ca: [fs.readFileSync(path.join(__dirname, './services/certificates/sspproviderca.crt'))],
+    requestCert: true,
+    rejectUnauthorized: true,
+}, sslvalidation
+).listen(sslPort);
 
 
 app.listen(port);
 
 // ROUTES
 const router = express.Router()
+const sslvalidationrouter = express.Router();
 
 const prefix = "sspBackend"
+const prefixsslvalidation = "ssl"
 
 app.use('/' + prefix, router);
+sslvalidation.use('/' + prefixsslvalidation, sslvalidationrouter);
 
 
 require("./controller/sspRegistrationController")(router, {});
 require("./controller/studentServiceController")(router, {});
+require("./controller/sslValidation")(sslvalidationrouter, {});
 
 
 
