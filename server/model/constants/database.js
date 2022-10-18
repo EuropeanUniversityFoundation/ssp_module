1c55d33d5bf60a70aeabd85f045341917f58685f
Bruno Pereira <bdpereira@uporto.pt>
2022-10-18 11:22:55 +0100

    remove data

--- server/model/constants/database.js
+++ server/model/constants/database.js
@@ -1,5 +1,4 @@
 module.exports = Object.freeze({
-    //DBURL: "mongodb+srv://ssp:Sezcs580jhmfSg1z@cluster0.kinfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     DBURL: "mongodb://mongo-db:27017/",
     DBNAME: "ssps",
     DBUSER: "backend",
